dd0715e033d7a468b9e055c96dd59d8ccf7f2921
David Lanzendörfer <david.lanzendoerfer@o2s.ch>
2018-01-20 05:49:53 +0800

    Adding install command

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,19 +8,22 @@ mkdir -p /build/qtflow/build
 cd /build/qtflow/build
 QT_SELECT=qt5 qmake ..
 make
-
+make install
 
 if [[ ! -d /build/graywolf ]]; then git clone https://github.com/leviathanch/graywolf.git /build/graywolf; fi
 cd /build/graywolf
 cmake .
 make
+make install
 
 if [[ ! -d /build/qrouter ]]; then git clone https://github.com/leviathanch/qrouter.git /build/qrouter; fi
 cd /build/qrouter
 ./configure
 make
+make install
 
 if [[ ! -d /build/yosys ]]; then git clone https://github.com/cliffordwolf/yosys.git /build/yosys; fi
 cd /build/yosys
 make
 make test
+make install
